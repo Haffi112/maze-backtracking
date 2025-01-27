f5898ca959fcd096dd9383e7f71aef7cfcba3f9b
haffi112 <haffi.e@gmail.com>
2025-01-27 20:22:41 +0000

    Fix tailwind stuff

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import MazeBacktracking from './components/MazeBacktracking'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 py-8">
-      <div className="container mx-auto">
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center py-8">
+      <div className="w-full max-w-4xl px-4">
         <h1 className="text-3xl font-bold text-center mb-8">Maze Backtracking Visualization</h1>
         <MazeBacktracking />
       </div>
